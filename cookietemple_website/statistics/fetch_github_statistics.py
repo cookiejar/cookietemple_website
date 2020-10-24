5dc2fd5b2eb29588971e300fc7d3da9d491df814
Imipenem <philipp_ehm@protonmail.com>
2020-10-24 23:43:44 +0200

    Working prototype chart of commits to cookietemple repo
    - need some bit of css styling (responsive!), and html stuff

--- cookietemple_website/statistics/fetch_github_statistics.py
+++ cookietemple_website/statistics/fetch_github_statistics.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 from github import Github
-from collections import OrderedDict
 import json
 
 
@@ -12,7 +11,7 @@ def fetch_ct_commits() -> None:
     g = Github(token)
     repo = g.get_repo('cookiejar/cookietemple')
     commits = repo.get_commits()
-    commit_dict = OrderedDict()
+    commit_dict = dict()
 
     for commit in commits:
         if commit.commit is not None:
@@ -22,10 +21,27 @@ def fetch_ct_commits() -> None:
                 commit_dict[date] += 1
             except KeyError:
                 commit_dict[date] = 1
+    # sort the commit dict by date in ascending order
+    commit_list = sorted(list(commit_dict.items()))
+    # convert to dict for easier JSON dumping
+    commit_dict = dict(commit_list)
+    # sum up all commits made up to (including) a date
+    sum_commits_until_date(commit_dict)
+    # dump data to json file to plot at stats subpage
     write_commits_to_json(commit_dict)
 
 
-def write_commits_to_json(commits_per_day: OrderedDict) -> None:
+def sum_commits_until_date(commits: dict) -> None:
+    """
+    For each date, calculate total sum of commits up to (and including) this date
+    :param commits: The commit date associated with the number of total commits per date
+    """
+    key_list = list(commits.keys())
+    for i in range(1, len(key_list)):
+        commits[key_list[i]] += commits[key_list[i - 1]]
+
+
+def write_commits_to_json(commits_per_day: dict) -> None:
     """
     Write the commits date and number to a local .json file
     """
@@ -35,4 +51,3 @@ def write_commits_to_json(commits_per_day: OrderedDict) -> None:
 
 if __name__ == '__main__':
     fetch_ct_commits()
-
